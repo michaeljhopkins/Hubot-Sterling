a8f2324f703db0d3a90546995a651cbce48571ca
Michael Hopkins <michael@appitventures.com>
2015-02-26 15:41:17 -0700

    adding many of reactivepixels scripts

---
+++ scripts/reactivepixel_clear.js
@@ -0,0 +1,61 @@
+// Description:
+//   Clear the page by moving the content upwards in the window.
+//
+// Dependencies:
+//   None
+//
+// Configuration:
+//   None
+//
+// Commands:
+//	Hubot clear desktop - clears the desktop avg line height
+//	Hubot clear mobile - clears the mobile avg line height
+//	Hubot clear tablet - clears the tablet avg line height
+//
+// Author:
+//	 dgerena aka Eli!
+//
+// Contributor:
+//	Holly Springsteen
+// 	hhspringsteen@gmail.com
+
+
+// I had wished to be able to pass in some specific character codes to push the page dependent on size of the browser had to settle for entering in spaces...
+function seeThrough(msg){
+    for(var esc = 0;esc < msg.count; esc++){
+        msg.send(" ");
+    }
+}
+// my filtering here is directly due to a misunderstanding about the extent to which i understood regex. Have to work on that for the foreseeable future...
+// regex can be awesome or a pain in the rear.
+module.exports = function(robot) {
+    return robot.respond(/clear\sdesktop$|clear\smobile$|clear\stablet$/i,function(msg){
+        //Filtering out the possible context to which someone might try to use the huClear.js/ Will work with the local and staging bots.
+        //Suggested fixes or upgrades are to extend this to work with anyones bot named anything. Or to function while somehow getting the viewing size.
+        //Still couldn't somehow pass in the robot to concat onto the string clear desktop to make dynamic for each robot its plugged into.
+
+        // Holly Springsteen
+        // allow for lower case robot calls with toLowerCase
+        var input = msg.message.text
+        switch(input.toLowerCase()){
+            //desktop clear
+            case"derpbot clear desktop":
+            case"hubot clear desktop":
+                msg.count = 25;
+                seeThrough(msg);
+                break;
+            //mobile clear
+            case"derpbot clear mobile":
+            case"hubot clear mobile":
+                msg.count = 11;
+                seeThrough(msg);
+                break;
+            //tablet clear
+            case"derpbot clear tablet":
+            case"hubot clear tablet":
+                msg.count = 21;
+                seeThrough(msg);
+                break;
+        }
+    });
+}
---
+++ scripts/reactivepixel_fact.js
@@ -0,0 +1,50 @@
+// Description:
+// Random Fact Generator
+//
+// Dependencies:
+// request
+//
+// Configuration:
+// none
+//
+// Commands:
+// Hubot random fact
+//
+// Author:
+// ndickiso
+
+
+var request = require('request');
+var ent = require('ent');
+
+//Requests random fact from mentalfloss.com api
+function getFact(msg){
+    request('http://mentalfloss.com/api/1.0/views/amazing_facts.json?limit=1&display_id=xhr&cb=0.7231947963021575', function (error, response, body) {
+
+        if (!error && response.statusCode < 300){
+
+            //Parse respsponse, find the fact, remove any html elements, stringify.
+            var jdata = JSON.parse(response.body);
+            var data = jdata[0].fact_body;
+            var regex = /(<([^>]+)>)/ig;
+
+            var result = data.replace(regex, "");
+
+            //Use ent to decode any html entities
+            result = ent.decode(result);
+
+            msg.send(result);
+
+        }else{
+            //Return error if api request goes wrong.
+            msg.send("Something went wrong here..");
+        }
+    })
+}
+
+//Listens for the exact match of random fact and calls random fact function.
+module.exports = function(robot) {
+    return robot.respond(/random fact/i, function(msg) {
+        getFact(msg);
+    });
+}
