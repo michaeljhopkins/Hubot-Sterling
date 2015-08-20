28421a3fc223df11bac783aecb886dab3fb88373
Michael Hopkins <mhopkins321@gmail.com>
2015-08-20 21:34:02 +0000

    adding trump

---
+++ scripts/responses.js
@@ -0,0 +1,207 @@
+module.exports = [
+    {
+        "keywords": ["america", "usa", "united states", "the us", "jobs", "economy"],
+        "messages": [
+            "When was the last time anybody saw us beating, let’s say, "
+            + "China in a trade deal? They kill us. I beat China all the time. -Donald Trump",
+            "The U.S. has become a dumping ground for everybody else’s problems. -Donald Trump",
+            "And our real unemployment is anywhere from 18 to 20 percent. Don't believe "
+            + "the 5.6. Don't believe it. -Donald Trump",
+            "I will be the greatest jobs president that God ever created."
+        ]
+    },
+    {
+        "keywords": ["debate", "republican debate", "gop", "presidential debate"],
+        "messages": [
+            "I don’t mind having a big, beautiful door on that wall — so people can come into this country [legally]",
+            "I don’t frankly have time for total political correctness. "
+            + "And to be honest you, this country doesn't have time either. "
+            + "This country is in big trouble. We don't win anymore. We lose to China. "
+            + "We lose to Mexico, both in trade and at the border. We lose to everybody. -Donald Trump",
+            "If it weren't for me you wouldn't even be talking about illegal immigration. This was "
+            + "not a subject on anybody's mind at my announcement, except the reporters are a very dishonest lot."
+        ]
+    },
+    {
+        "keywords": ["mexico", "mexican", "latino", "border"],
+        "messages": [
+            "When Mexico sends its people, they’re not sending their best. "
+            + "They’re sending people that have lots of problems. -Donald Trump",
+            "I will build a great wall — and nobody builds walls better than me, "
+            + "believe me —and I’ll build them very inexpensively. I will build, "
+            + "a great, great wall on our southern border, and I will make Mexico "
+            + "pay for that wall. Mark my words. -Donald Trump",
+            "They’re bringing drugs. They’re bringing crime. They’re rapists. "
+            + "And some, I assume, are good people. -Donald Trump",
+            "Mexico is becoming the new China!",
+            "Mexico’s totally corrupt government looks horrible with El Chapo’s"
+            + " escape—totally corrupt. U.S. paid them $3 billion."
+        ]
+    },
+    {
+        "keywords": ["jeb", "wall", "borders", "immigra", "illegal"],
+        "messages": [
+            "Jeb Bush has to like the Mexican illegals because of his wife. -Donald Trump",
+            "A nation WITHOUT BORDERS is not a nation at all. We must have a wall. "
+            + "The rule of law matters. Jeb just doesn’t get it."
+        ]
+    },
+    {
+        "keywords": ["china", "asia", "japan", "korea"],
+        "messages": [
+            "When did we beat Japan at anything? They send their cars over "
+            + "here by the millions, and what do we do? When was the last time "
+            + "you saw a Chevrolet in Tokyo? It doesn't exist, folks. -Donald Trump",
+            "I know the Chinese. I've made a lot of money with the Chinese. I understand "
+            + " the Chinese mind."
+        ]
+    },
+    {
+        "keywords": ["money", "billion", "million", "dollar"],
+        "messages": [
+            "That's one of the nice things. I mean, part of the beauty of me "
+            + "is that I'm very rich. So if I need $600 million, I can put $600 " 
+            + "million myself. That's a huge advantage. I must tell you, that's a " 
+            + "huge advantage over the other candidates. -Donald Trump",
+            "Money was never a big motivation for me, except as a way to keep score."
+            + " The real excitement is playing the game."
+        ]
+    },
+    {
+        "keywords": ["trump", "donald", "president"],
+        "messages": [
+            "Do you mind if I sit back a little? Because your breath is very bad. -Donald Trump",
+            "Sorry losers and haters, but my I.Q. is one of the highest—and you all "
+            + "know it! Please don’t feel so stupid or insecure. It’s not your fault. -Donald Trump",
+            "As everybody knows, but the haters & losers refuse to acknowledge, I do not "
+            + "wear a “wig.” My hair may not be perfect, but it’s mine."
+        ]
+    },
+    {
+        "keywords": ["isis", "middle east", "iran", "iraq", "israel", "terror"],
+        "messages": [
+            "I would bomb the hell out of those oilfields. I wouldn't send many "
+            + "troops because you won't need 'em by the time I'm finished. -Donald Trump",
+            "They just built a hotel in Syria. Can you believe this? They built a hotel. "
+            + "When I have to build a hotel, I pay interest. They don't have to pay interest, "
+            + "because they took the oil that, when we left Iraq, I said we should've taken. -Donald Trump",
+            "If Iran was a stock you should go out and buy it, cause you'll quadruple."
+        ]
+    },
+    {
+        "keywords": ["mccain", "john", "veterans", "army", "war", "hero"],
+        "messages": [
+            "I'm very disappointed in John McCain because the vets are horribly treated "
+            + "in this country. I'm fighting for the vets. I've done a lot for the vets. -Donald Trump",
+            "He's not a war hero. He's a war hero because he was captured. I like people that "
+            + "weren't captured, OK, I hate to tell you."
+        ]
+    },
+    {
+        "keywords": ["plan", "future", "campaign", "going", "to do"],
+        "messages": [
+            "I try to learn from the past, but I plan for the future by focusing exclusively on the present."
+            + "That's where the fun is. -Donald Trump",
+            "You have to think anyway, so why not think big?",
+            "In the end, you're measured not by how much you undertake but by what you finally accomplish."
+        ]
+    },
+    {
+        "keywords": ["obama"],
+        "messages": [
+            "He may have one but there's something on that, maybe religion, maybe it says he is a Muslim. "
+            + "I don't know. Maybe he doesn't want that. Or he may not have one. I will tell you this: "
+            + "if he wasn't born in this country, it's one of the great scams of all time. -Donald Trump",
+            "An 'extremely credible source' has called my office and told me that Barack Obama's birth "
+            + "certificate is a fraud."
+        ]
+    },
+    {
+        "keywords": ["oil", "price"],
+        "messages": [
+            "We have nobody in Washington that sits back and said, you're not going to raise that fucking price."
+        ]
+    },
+    {
+        "keywords": ["muammar", "gadaffi"],
+        "messages": [
+            "I dealt with Gaddafi. I rented him a piece of land. He paid me more for "
+            + "one night than the land was worth for two years, and then I didn't let him use the land. "+
+            "That's what we should be doing. I don't want to use the word 'screwed,'' but I screwed him. "
+            + "That's what we should be doing."
+        ]
+    },
+    {
+        "keywords": ["rosie", "o'donell"],
+        "messages": [
+            "Rosie is crude, rude, obnoxious and dumb - other than that I like her very much!"
+        ]
+    },
+    {
+        "keywords": ["bush", "george bush"],
+        "messages": [
+          "You mean George Bush sends our soldiers into combat, they are severely wounded, and then he" +
+          "wants $120,000 to make a boring speech to them?",
+          "You can’t have Bush. The last thing we need is another Bush"
+        ]
+    },
+    {
+        "keywords": ["kasich", "john kasich"],
+        "messages":[
+            "What people don’t know about Kasich- he was a managing partner of the horrendous Lehman Brothers"
+            + " when it totally destroyed the economy!",
+            "I now see John Kasich from Ohio- who is desperate to run- is using my line"
+            + " 'Make America Great Again'. Typical pol- no imagination!"
+        ]
+    },
+    {
+        "keywords": ["republican", "republican party", "nominee", "candidate"],
+        "messages":[
+            "I’m talking about a lot of leverage. I want to win and we will win. -Donald Trump",
+            "I can't say that I have to respect that person who wins. If I'm"
+            + "the nominee, I pledge I will not run as an independent. -Donald Trump",
+            "The other candidates — they went in, they didn’t know the air conditioning didn’t work. They sweated"
+            + "like dogs. They didn’t know the room was too big because they didn’t have anybody there. How are they"
+            + "gonna beat ISIS? I don’t think it’s gonna happen."
+        ]
+    },
+    {
+        "keywords": ["free trade", "trade"],
+        "messages": [
+            "Free trade is terrible. Free trade can be wonderful if you have smart people. But we have stupid people. -Donald Trump",
+        ]
+    },
+    {
+        "keywords": ["twitter", "social"],
+        "messages": [
+            "My Twitter account has over 3.5 million followers. Beat that!",
+        ]
+    },
+    {
+        "keywords": ["win", "lose"],
+        "messages": [
+            "Sometimes by losing a battle you find a new way to win the war."
+        ]
+    },
+    {
+        "keywords": ["hillary", "clinton"],
+        "messages": [
+            "Hillary Clinton was the worst Secretary of State in the history of the "
+            + "United States. There's never been a Secretary of State so bad as Hillary. "
+            + "The world blew up around us. We lost everything, including all relationships. "
+            + "There wasn't one good thing that came out of that administration or her being "
+            + "Secretary of State."
+        ]
+    },
+    {
+        "keywords": ["gay", "marriage"],
+        "messages": [
+            "It's like in golf. A lot of people — I don't want this to sound trivial — but a lot of people are"
+            +"switching to these really long putters, very unattractive. It’s weird. You see these great"
+            +"players with these really long putters, because they can't sink three-footers anymore. And, I"
+            +"hate it. I am a traditionalist. I have so many fabulous friends who happen to be gay, but I"
+            +" am a traditionalist."
+        ]
+    }
+
+];
